dcc49c5fc1093dcd0a6b9c56a6c44becf8800915
tin6150 <tin_bofh_202206>
2024-04-13 11:03:52 -0700

    expanded pip install list using requirements.txt, extracted from ETA Pilates

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,8 +7,15 @@ apt-get -y install python-pip
 
 # cd /
 
+date
 
 pip install numpy pandas
+echo $?
+echo "==================================="
+pip install -r requirements.txt
+echo $?
+echo "==================================="
+date
 
-pip list
+pip list > pip_list.after_install.txt
 
